92df80c439ede1788d3853252578c85be24faebd
MeruyertZhaketova <zhaketova.m@icloud.com>
2026-01-30 12:43:14 +0500

    upd

---
+++ prac01/1.py
@@ -0,0 +1,34 @@
+if 5 > 2:
+  print("Five is greater than two!")
+
+  #its a single-line comment
+  """ its 
+  a some"""
+
+
+print("Hello World!", end=" ")
+print("добавляет встроку а не вниз")
+
+x = 8
+y = "тлтдото"
+print(x)
+print(y)
+
+z = float(3)
+print(type(z))
+
+variable=3
+VarIable=5
+_var=6
+_j_hp=8
+
+#collection
+
+fruits = ["apple", "banana", "cherry"]
+x,y,z=fruits
+print(x)
+print(y)
+print(z)
+
+#or print(x,y,z) or print(x+y+z)
+
