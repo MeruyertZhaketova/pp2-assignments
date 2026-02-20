c6e53fb71c727cc4ee894055e07b0b35f518cc36
MeruyertZhaketova <zhaketova.m@icloud.com>
2026-02-20 12:49:54 +0500

    Complete Practice 3: Python functions, lambda, classes, and inheritance examples

---
+++ git3/functions/1.py
@@ -0,0 +1,11 @@
+def my_fun():
+    print("hello" + " 12")
+
+my_fun()
+
+def fahrenheit_to_celsius(fahrenheit):
+  return (fahrenheit - 32) * 5 / 9
+
+print(fahrenheit_to_celsius(77))
+print(fahrenheit_to_celsius(95))
+print(fahrenheit_to_celsius(50))
---
+++ git3/lambda/2.py
@@ -0,0 +1,18 @@
+"""A lambda function is a small anonymous function.
+
+A lambda function can take any number of arguments, but can only have one expression."""
+x = lambda a : a + 10
+print(x(5))
+
+x = lambda a, b : a * b
+print(x(5, 6))
+
+x = lambda a, b, c : a + b + c
+print(x(5, 6, 2))
+
+def myfunc(n):
+  return lambda a : a * n
+
+numbers = [1, 2, 3, 4, 5]
+doubled = list(map(lambda x: x * 2, numbers))
+print(doubled)
